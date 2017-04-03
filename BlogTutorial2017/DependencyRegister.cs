3c896d58bf4bb815aedbcb0a8b751166bedbb32d
defazze <defazze@gmail.com>
2017-04-03 23:26:12 +0300

    Перенос регистрации зависимостей в корневой проект

---
+++ BlogTutorial2017/DependencyRegister.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using BlogTutorial.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogTutorial2017
+{
+    public static class DependencyRegister
+    {
+        public static void AddIdentityStore(this ContainerBuilder builder)
+        {
+            builder.AddScoped<ApplicationUserStore>();
+            builder.AddScoped<ApplicationRoleStore>();
+        }
+
+        private static void AddScoped<T>(this ContainerBuilder builder)
+        {
+            builder.RegisterType<T>().InstancePerLifetimeScope();
+        }
+    }
+}
